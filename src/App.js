57c4c17ce3270c3d71435a79639507d913ad8c80
TLaloo <Tricia_Laloo@quadra.ca>
2023-02-23 16:05:23 -0500

    Added Styling

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
         </header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="sunset" />
         </main>
         <footer className="App-footer">
           <small>Coded by SheCodes</small>
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,13 +1,14 @@
 import React from "react";
+import "./Phonetic.css";
 
 export default function Phonetic(props) {
   console.log(props.phonetic);
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+      <a href={props.phonetic.audio} target="_blank">
         Listen
       </a>
-      {props.phonetic.text}
+      <span className="text">{props.phonetic.text}</span>
     </div>
   );
 }
